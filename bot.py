8b8eda2f2a8e9a0e1718bac9e20d1d7a7ebec769
Divide-By-0 <aayushg@mit.edu>
2024-01-01 18:58:24 -0800

    add error handling and fix typo

--- bot.py
+++ bot.py
@@ -17,45 +17,53 @@ intents = discord.Intents.all()
 bot = discord.Bot(command_prefix="!", intents=intents)
 
 @bot.slash_command(description="Move messages to threads")
-async def move(ctx, num_messages: int, thread_name: str, silent: Option(str, "Should I respond on success?", choices=["silent","loud"], required=False, defualt="loud"),privacy: Option(str, "Is the thread public or private?",choices=["private","public"], required=False, defualt="public")):
-    # Acknowledge the command immediately
-    await ctx.defer()
-    
-    # Get the last num_messages messages from the channel
-    messages = []
-    thread_name = thread_name.lower()
-    async for message in ctx.channel.history(limit=num_messages):
-        if message.author != bot.user:
-            messages.append(message)
+async def move(ctx, num_messages: int, thread_name: str, silent: Option(str, "Should I respond on success?", choices=["silent","loud"], required=False, default="loud"), privacy: Option(str, "Is the thread public or private?",choices=["private","public"], required=False, defualt="public")):
+    try:
+        # Acknowledge the command immediately
+        await ctx.defer()
         
-    threadExists = False
-    for thread in ctx.guild.threads:
-        if thread.name.lower() == thread_name.lower():
-            threadExists = thread
+        # Get the last num_messages messages from the channel
+        messages = []
+        thread_name = thread_name.lower()
+        self_messages = 0
+        async for message in ctx.channel.history(limit=num_messages):
+            if message.author != bot.user:
+                messages.append(message)
+            else:
+                self_messages += 1
+            
+        threadExists = False
+        for thread in ctx.guild.threads:
+            if thread.name.lower() == thread_name.lower():
+                threadExists = thread
 
-    # Delete the last num_messages messages from the channel
-    finalMessageList = messages #existingMessages + messages
-    finalMessageList.sort(key=sSort)
+        # Delete the last num_messages messages from the channel
+        finalMessageList = messages #existingMessages + messages
+        finalMessageList.sort(key=sSort)
 
-    # Create a new thread
-    thread = False
-    if threadExists:
-        thread = threadExists
-    else:
-        if privacy == "public":
-            whatsUp = await ctx.send("Creating thread "+thread_name+" and moving "+ str(num_messages))
-            thread = await whatsUp.create_thread(name=thread_name)
+        # Create a new thread
+        thread = False
+        if threadExists:
+            thread = threadExists
         else:
-            thread = await ctx.channel.create_thread(name=thread_name)
-# Move the messages to the new thread
-    for message in finalMessageList:
-        content = message.author.mention + " said: \n" + message.content
-        await thread.send(content=content,files=[await f.to_file() for f in message.attachments])
-    await ctx.channel.delete_messages(messages)
-    
-    if silent == "loud":
-      await ctx.respond(str(num_messages) + " moved to " + thread_name)
-    
-# Deploy a flask server to comply with Render's requirements
+            if privacy == "public":
+                whatsUp = await ctx.send("Creating thread "+thread_name+" and moving "+ str(num_messages))
+                thread = await whatsUp.create_thread(name=thread_name)
+            else:
+                thread = await ctx.channel.create_thread(name=thread_name)
+                
+        # Move the messages to the new thread
+        for message in finalMessageList:
+            content = message.author.mention + " said: \n" + message.content
+            await thread.send(content=content,files=[await f.to_file() for f in message.attachments])
+        await ctx.channel.delete_messages(messages)
+        
+        if silent == "loud":
+            await ctx.respond(str(num_messages) + " moved to " + thread_name + ", " + str(self_messages) + "bot messages ignored")
+
+    except Exception as e:
+        await ctx.respond(f"An error occurred: {e}")
+        print(e)
+        
 if __name__ == "__main__":  
     bot.run(TOKEN)
